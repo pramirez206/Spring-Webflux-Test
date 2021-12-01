25eea5297be0d156f375c6523fc0bec338f6d1e9
pramirez206 <pramirez206@unab.edu.co>
2021-12-01 16:52:57 -0500

    include tests

---
+++ src/test/java/com/example/Spring/Webflux/Test/ServicioTest.java
@@ -0,0 +1,97 @@
+package com.example.Spring.Webflux.Test;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ServicioTest {
+
+    @Autowired
+    Servicio servicio;
+
+    @Test
+    void testMono() {
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+
+    @Test
+    void testVarios() {
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+
+    }
+
+    @Test
+    void testVariosLento() {
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1)).verifyComplete();
+    }
+
+    @Test
+    void testTodosFiltro() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .withVirtualTime(() -> Flux.interval(Duration.ofSeconds(1)).take(2))
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(1))
+                .expectNext(0L)
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void testTodosFiltroError() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .create(source)
+                .expectNextCount(4)
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
+                        throwable.getMessage().equals("Mensaje de Error")
+                ).verify();
+    }
+
+    @Test
+    void droppedElements() {
+        Flux<Integer> source = servicio.emisorConParada();
+
+        StepVerifier.create(source)
+                .expectNext(2)
+                .expectComplete()
+                .verifyThenAssertThat()
+                .hasDropped(4)
+                .tookLessThan(Duration.ofMillis(1050));
+    }
+
+    @Test
+    void testPublisher() {
+        TestPublisher<String> source = TestPublisher
+                .<String>create()
+                .next("Primero", "Segundo", "Tercero")
+                .error(new RuntimeException("Mensaje de Error"));
+
+        StepVerifier.create(source)
+               .expectErrorMatches(throwable -> throwable instanceof RuntimeException &&
+                        throwable.getMessage().equals("Mensaje de Error"))
+               .verify();
+    }
+}
---
+++ src/test/java/com/example/Spring/Webflux/Test/UppercaseConverterTest.java
@@ -0,0 +1,25 @@
+package com.example.Spring.Webflux.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UppercaseConverterTest {
+
+    final TestPublisher<String> testPublisher = TestPublisher.create();
+
+    @Test
+    void testUpperCase() {
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
+    }
+
+}
